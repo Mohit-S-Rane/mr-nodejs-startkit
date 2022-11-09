1066f38829dc1fb71d5128ee3d0b82609df53a3a
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-11-09 15:38:53 +0530

    Pagination added

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -7,29 +7,53 @@ import * as Jwt from "jsonwebtoken";
 import { getEnvironmentVariable } from "../environment/env";
 
 export class PostController {
-    static addPost(req,res,next){
-        const userId = req.user.user_id;
-        const content = req.body.content;
-        const post = new Post({
-            user_id: userId,
-            content: content,
-            created_at: new Date(),
-            updated_at: new Date()
-        })
-        post.save().then((post)=>{
-            res.send(post);
-        }).catch((err)=>{
-            next(err)
-        })
-    }
+  static addPost(req, res, next) {
+    const userId = req.user.user_id;
+    const content = req.body.content;
+    const post = new Post({
+      user_id: userId,
+      content: content,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    post
+      .save()
+      .then((post) => {
+        res.send(post);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-    static async getPostByUser(req,res,next) {
-        const userId = req.user.user_id;
-        try {
-            const posts = await Post.find({user_id: userId}, {user_id: 0, __v: 0}).populate('comments').exec();
-            res.send(posts);
-        } catch (e) {
-            next(e)
-        }
-    }   
+  static async getPostByUser(req, res, next) {
+    const userId = req.user.user_id;
+    const page = parseInt(req.query.page) || 1;
+    const perPage = 2;
+    let currentPage = page;
+    let prevPage = page === 1 ? null : page - 1;
+    let pageToken = page + 1;
+    let totalPages;
+    try {
+      const postCount = await Post.countDocuments({ user_id: userId });
+      totalPages = Math.ceil(postCount / perPage);
+      if (totalPages === page || totalPages === 0) {
+        pageToken = null;
+      }
+      const posts = await Post.find({ user_id: userId }, { __v: 0, user_id: 0 })
+        .populate("comments")
+        .skip(perPage * page - perPage)
+        .limit(perPage);
+
+      res.json({
+        post: posts,
+        pageToken: pageToken,
+        totalpages: totalPages,
+        currentPage: currentPage,
+        prevPage: prevPage,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
 }
